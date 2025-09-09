7eaa1728544880e8a328333dd654814d1a072549
xusha1 <xu.sha1@byd.com>
2025-09-09 16:17:11 +0800

    将聚类的点数以及距离参数写在launch中

--- amr_reflective_circle/launch/reflector_detector_circle.launch.py
+++ amr_reflective_circle/launch/reflector_detector_circle.launch.py
@@ -23,7 +23,17 @@ def generate_launch_description():
         default_value='30.0',
         description='最大弧线特征'
     )
-
+    cluster_eps_arg = DeclareLaunchArgument(
+        'cluster_eps',
+        default_value='0.064',
+        description='聚类的距离'
+    )
+    
+    min_cluster_points_arg = DeclareLaunchArgument(
+        'min_cluster_points',
+        default_value='5.0',
+        description='聚类最小点数'
+    )
     # 定义节点
     reflector_detector_circle = Node(
         package='amr_reflective_circle',
@@ -34,6 +44,8 @@ def generate_launch_description():
             'intensity_threshold_use': LaunchConfiguration('intensity_threshold_use'),
             'arc_threshold': LaunchConfiguration('arc_threshold'),
             'max_arc_feature': LaunchConfiguration('max_arc_feature'),
+            'cluster_eps': LaunchConfiguration('cluster_eps'),
+            'min_cluster_points': LaunchConfiguration('min_cluster_points'),
         }]
     )
 
@@ -42,6 +54,8 @@ def generate_launch_description():
         intensity_threshold_use_arg,
         arc_threshold_arg,
         max_arc_feature_arg,
+        cluster_eps_arg,
+        min_cluster_points_arg,
         # 节点
         reflector_detector_circle
     ])
