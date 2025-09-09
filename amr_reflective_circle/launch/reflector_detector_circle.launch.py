fba494550b9374e16dda035febd7cdb91bb474ce
shen.sikang <shen.sikang@byd.com>
2025-09-09 10:24:40 +0800

    add launch params

--- amr_reflective_circle/launch/reflector_detector_circle.launch.py
+++ amr_reflective_circle/launch/reflector_detector_circle.launch.py
@@ -1,16 +1,47 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 import os
 
 def generate_launch_description():
+    # 声明launch参数
+    intensity_threshold_use_arg = DeclareLaunchArgument(
+        'intensity_threshold_use',
+        default_value='1000',
+        description='强度阈值'
+    )
+    
+    arc_threshold_arg = DeclareLaunchArgument(
+        'arc_threshold',
+        default_value='0.1',
+        description='弧线阈值'
+    )
+    
+    max_arc_feature_arg = DeclareLaunchArgument(
+        'max_arc_feature',
+        default_value='30.0',
+        description='最大弧线特征'
+    )
+
     # 定义节点
     reflector_detector_circle = Node(
         package='reflective_circle',
         executable='reflector_detector_circle',
         name='reflector_detector_circle',
-        output='screen'
+        output='screen',
+        parameters=[{
+            'intensity_threshold_use': LaunchConfiguration('intensity_threshold_use'),
+            'arc_threshold': LaunchConfiguration('arc_threshold'),
+            'max_arc_feature': LaunchConfiguration('max_arc_feature'),
+        }]
     )
 
     return LaunchDescription([
+        # 参数声明
+        intensity_threshold_use_arg,
+        arc_threshold_arg,
+        max_arc_feature_arg,
+        # 节点
         reflector_detector_circle
     ])
