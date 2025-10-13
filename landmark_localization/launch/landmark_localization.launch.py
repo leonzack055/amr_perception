b24fc73c67b679ac19d08777fc98563d98fdfe65
wu.ying18@byd.com <wu.ying18@byd.com>
2025-10-13 15:36:59 +0800

    更新反光柱检测参数

--- landmark_localization/launch/landmark_localization.launch.py
+++ landmark_localization/launch/landmark_localization.launch.py
@@ -47,7 +47,7 @@ def generate_launch_description():
 
     intensity_threshold_use_arg = DeclareLaunchArgument(
         'intensity_threshold_use',
-        default_value='1600',
+        default_value='1000',
         description='强度阈值'
     )
 
